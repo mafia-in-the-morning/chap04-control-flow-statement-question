9eef2b1f5376f3008755e2989aad643a5cafb771
kokojihye <jihyevivianako@gmail.com>
2023-05-04 14:06:06 +0900

    //level2.normal [문제 1] 특정한 값 사이의 정수의 짝수/홀수 파악하기 [문제 2] BMI계산 후 값에 따라 안내하

--- src/main/java/com/ohgiraffers/section01/conditional/level02/normal/Application2.java
+++ src/main/java/com/ohgiraffers/section01/conditional/level02/normal/Application2.java
@@ -1,5 +1,7 @@
 package com.ohgiraffers.section01.conditional.level02.normal;
 
+import java.util.Scanner;
+
 public class Application2 {
 	
 	public static void main(String[] args) {
@@ -14,7 +16,41 @@ public class Application2 {
 		 * 
 		 * 계산 예시) BMI = 67 / (1.7 * 1.7)
 		 * */
-		
+
+		Scanner sc = new Scanner(System.in);
+		int height;
+		int weight;
+		int BMI;
+
+		System.out.print("키를 입력하세요(cm): ");
+		height = sc.nextInt();
+
+		System.out.print("몸무게를 입력하세요(kg): ");
+		weight = sc.nextInt();
+
+		double heightD = height * 0.01;
+		BMI = getBMI(weight, heightD);
+
+		if (BMI < 20){
+			System.out.println("당신은 저체중입니다.");
+		}
+		else if(BMI >= 20 || BMI < 25){
+			System.out.println("당신은 정상체중입니다.");
+		}
+		else if(BMI >= 25 || BMI < 30){
+			System.out.println("당신은 과체중입니다.");
+		}
+		else{
+			System.out.println("당신은 과체중입니다.");
+		}
+	}
+
+	public static int getBMI(int weight, double height){
+		// BMI 계산 방법은 체중(kg) / (신장(m) * 신장(m)) 이다.
+		int BMI;
+		BMI = weight / (int)(height * height);
+
+		return BMI;
 	}
 	
 }
