8dce4dfd5ad0aa7aa42d26c7d0a282f92a970eea
kokojihye <jihyevivianako@gmail.com>
2023-05-08 11:25:45 +0900

    //section02 - level3.hard [문제 1] 입력받은 숫자가 소수인지 구분하기 [문제 1 풀이] 2보다 큰 정수를 입력하지 않으면 프로그램을 종료하지 않고 다시 입력받도록 하는 부분이 어려웠다. do-while문을 이용해서 풀어냈는데 사용이 어색해서 여러번 연습해야 할 것 같다.

--- src/main/java/com/ohgiraffers/section02/looping_and_branching/level03/hard/Application1.java
+++ src/main/java/com/ohgiraffers/section02/looping_and_branching/level03/hard/Application1.java
@@ -1,5 +1,6 @@
 package com.ohgiraffers.section02.looping_and_branching.level03.hard;
 
+import java.util.*;
 public class Application1 {
 
 	public static void main(String[] args) {
@@ -20,7 +21,31 @@ public class Application1 {
 		 * -- 출력 예시 --
 		 * 소수다.
 		 * */
-		
-	}
 
+		Scanner scanner = new Scanner(System.in);
+		int number;
+		boolean isPrime;
+
+		do {
+			System.out.print("2보다 큰 정수를 하나 입력하세요: ");
+			number = scanner.nextInt();
+			if(number <= 2){
+				System.out.println("잘못 입력하셨습니다. 다시 입력하세요.");
+			}
+		} while (number <= 2);
+
+		isPrime = true;
+		for (int i = 2; i <= Math.sqrt(number); i++) {
+			if (number % i == 0) {
+				isPrime = false;
+				break;
+			}
+		}
+
+		if (isPrime) {
+			System.out.println("소수다.");
+		} else {
+			System.out.println("소수가 아니다.");
+		}
+	}
 }
