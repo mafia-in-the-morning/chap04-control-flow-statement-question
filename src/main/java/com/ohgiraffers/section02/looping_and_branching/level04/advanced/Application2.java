698626cc76da45ca8e936084d5bc4160c39faf5e
kokojihye <jihyevivianako@gmail.com>
2023-05-08 11:33:22 +0900

    //section02 - level4.advanced [문제 2] 거스름돈 지폐와 동전 개수 구하기
    [문제 2 풀이] 좀 노가다로 풀었다... 다시 풀어볼 예정

--- src/main/java/com/ohgiraffers/section02/looping_and_branching/level04/advanced/Application2.java
+++ src/main/java/com/ohgiraffers/section02/looping_and_branching/level04/advanced/Application2.java
@@ -1,5 +1,7 @@
 package com.ohgiraffers.section02.looping_and_branching.level04.advanced;
 
+import java.util.Scanner;
+
 public class Application2 {
 
 	public static void main(String[] args) {
@@ -26,7 +28,58 @@ public class Application2 {
 		 * ============================
 		 * 거스름돈 : 77660원
 		 * */
-		
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("받으신 금액을 입력하세요: ");
+		int receivedMoney = sc.nextInt();
+		System.out.print("상품 가격을 입력하세요: ");
+		int productPrice = sc.nextInt();
+		int change = receivedMoney - productPrice;
+
+		int change50000;
+		int change10000;
+		int change5000;
+		int change1000;
+		int change500;
+		int change100;
+		int change50;
+		int change10;
+
+		change50000 = change / 50000;
+		change = change %= 50000;
+
+		change10000 = change / 10000;
+		change = change %= 10000;
+
+		change5000 = change / 5000;
+		change = change %= 5000;
+
+		change1000 = change / 1000;
+		change = change %= 1000;
+
+		change500 = change / 500;
+		change = change %= 500;
+
+		change100 = change / 100;
+		change = change %= 100;
+
+		change50 = change / 50;
+		change = change %= 50;
+
+		change10 = change / 10;
+		change = change %= 10;
+
+		System.out.println("===========================");
+		System.out.println("50000원권 지폐 " + change50000 + "장");
+		System.out.println("10000원권 지폐 " + change10000 + "장");
+		System.out.println("5000원권 지폐 " + change5000 + "장");
+		System.out.println("1000원권 지폐 " + change1000 + "장");
+		System.out.println("500원권 동전 " + change500 + "개");
+		System.out.println("100원권 동전 " + change100 + "개");
+		System.out.println("50원권 동전 " + change50 + "개");
+		System.out.println("10원권 동전 " + change10 + "개");
+		System.out.println("===========================");
+		System.out.println("거스름돈: "+ (receivedMoney - productPrice));
 	}
 
 }
